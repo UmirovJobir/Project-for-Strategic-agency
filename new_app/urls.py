dfdf9496369ca0d2b4c2033433f9154a71871c81
Doston Kanaev <d.kanaev@sda.local>
2022-11-01 12:37:02 +0300

    urls change

--- new_app/urls.py
+++ new_app/urls.py
@@ -3,7 +3,7 @@ from .views import Detail, Country_view, Product_view, Data, SaveDataView
 
 
 urlpatterns = [
-    path('detail/', Detail.as_view()),
+    #path('detail/', Detail.as_view()),
     path('products/', Product_view.as_view()),
     path('countries/', Country_view.as_view()),
     path('data/', Data.as_view()),
